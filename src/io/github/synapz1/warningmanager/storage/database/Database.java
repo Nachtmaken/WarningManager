6a9d56341482066a36df1cd028e1a648554ebd9f
Fridgey <jeremylugo110997@gmail.com>
2016-05-29 02:24:56 -0400

    Some more changes to MySQL. Need to do some testing.

--- src/io/github/synapz1/warningmanager/storage/database/Database.java
+++ src/io/github/synapz1/warningmanager/storage/database/Database.java
@@ -2,15 +2,18 @@ package io.github.synapz1.warningmanager.storage.database;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * Created by Jeremy(Refrigerbater) on 5/9/2016.
+ * Created by Jeremy Lugo(Refrigerbater) on 5/9/2016.
  */
 public class Database
 {
@@ -67,7 +70,14 @@ public class Database
     public void createTable() throws SQLException
     {
         openConnection();
-        PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS " + statsTable + ";");
+        PreparedStatement statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS " + statsTable
+                    + " id int NOT NULL AUTO_INCREMENT"
+                    + ",username VARCHAR(16) NOT NULL"
+                    + ",type VARCHAR(16) NOT NULL"
+                    + ",reason VARCHAR(255) NOT NULL"
+                    + ",sender VARCHAR(16) NOT NULL"
+                    + ",date DATETIME NOT NULL"
+                    + ",PRIMARY KEY (id);");
         statement.executeUpdate();
     }
 
@@ -84,14 +94,66 @@ public class Database
     public void updateDatabase(FileConfiguration config) throws SQLException
     {
         openConnection();
-        PreparedStatement statement = connection.prepareStatement("");
+        // Clear the warnings so that nothing gets added that is already in the database.
+        PreparedStatement clear = connection.prepareStatement("TRUNCATE TABLE " + statsTable);
+        clear.executeUpdate();
+        for (String username : config.getKeys(false))
+        {
+            ConfigurationSection section = config.getConfigurationSection(username + ".Warning");
+            for (String type : section.getKeys(false))
+            {
+                try
+                {
+                    SimpleDateFormat format = new SimpleDateFormat("MM-dd-YYYY HH:mm:ss");
+                    java.util.Date date = format.parse(section.getString(type + ".Date"));
+                    PreparedStatement statement = connection.prepareStatement("INSERT INTO " + statsTable
+                                + " '" + username + "'"
+                                + ",'" + type + "'"
+                                + ",'" + section.getString(type + ".Reason")
+                                + ",'" + section.getString(type + ".Sender")
+                                + "'" + date + ";");
+                } catch(ParseException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
-    public void deleteRow(String playername) throws SQLException
+    public FileConfiguration buildConfig() throws SQLException
     {
         openConnection();
-        PreparedStatement statement = connection.prepareStatement("DELETE FROM " + statsTable + " WHERE(playername = '" + playername + "');");
-        statement.executeUpdate();
+        FileConfiguration config = new YamlConfiguration();
+        Set<String> players = new HashSet<String>();
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM " + statsTable);
+        ResultSet set = statement.executeQuery();
+        while (set.next())
+        {
+            String username = set.getString("username");
+            players.add(username);
+        }
+
+        for (String username : players)
+        {
+            statement = connection.prepareStatement("SELECT * FROM " + statement + " WHERE username = '" + username + "'");
+            set = statement.executeQuery();
+            int c = 0;
+            while (set.next())
+            {
+                c++;
+                config.set(username + ".TotalWarnings", c);
+                String type = set.getString("type");
+                String reason = set.getString("reason");
+                String sender = set.getString("sender");
+                Date date = set.getDate("date");
+                config.set(username + "." + c + ".Type", type);
+                config.set(username + "." + c + ".Sender", sender);
+                config.set(username + "." + c + ".Date", date);
+                config.set(username + "." + c + ".Reason", reason);
+            }
+        }
+
+        return config;
     }
 
     public boolean doesTableExist() throws SQLException
